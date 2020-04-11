7b93134391dd4d512280a5eb42c52f9bcb3ca655
feng <feng@mbp.local>
2020-04-12 00:43:13 +0800

    添加按钮的隐藏和显示

--- app/src/main/java/com/inkeast/wuxiaworld/MainActivity.java
+++ app/src/main/java/com/inkeast/wuxiaworld/MainActivity.java
@@ -5,6 +5,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -15,6 +16,8 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Menu mMenu;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,13 +26,32 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.navigation_home, R.id.navigation_bookmark, R.id.navigation_history).build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        final NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+        navView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.navigation_home:
+                        showMenu();
+                        break;
+                    case R.id.navigation_bookmark:
+                        hiddenMenu();
+                        break;
+                    case R.id.navigation_history:
+                        hiddenMenu();
+                        break;
+                }
+                return NavigationUI.onNavDestinationSelected(item, navController);
+            }
+        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        mMenu = menu;
         getMenuInflater().inflate(R.menu.tool_bar_menu, menu);
         return true;
     }
@@ -38,9 +60,24 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == R.id.action_add_bookmark) {
-            Toast.makeText(this,"添加书签",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "添加书签", Toast.LENGTH_SHORT).show();
         }
         return super.onOptionsItemSelected(item);
     }
 
+    private void hiddenMenu() {
+        if (null != mMenu) {
+            for (int i = 0; i < mMenu.size(); i++) {
+                mMenu.getItem(i).setVisible(false);
+            }
+        }
+    }
+
+    private void showMenu() {
+        if (null != mMenu) {
+            for (int i = 0; i < mMenu.size(); i++) {
+                mMenu.getItem(i).setVisible(true);
+            }
+        }
+    }
 }
