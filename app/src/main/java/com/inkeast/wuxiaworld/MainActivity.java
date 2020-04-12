8708046aa2ddfc07bb588cc1f154de9afec37a1f
feng <feng@mbp.local>
2020-04-12 18:33:01 +0800

    navigateToHome

--- app/src/main/java/com/inkeast/wuxiaworld/MainActivity.java
+++ app/src/main/java/com/inkeast/wuxiaworld/MainActivity.java
@@ -3,14 +3,11 @@ package com.inkeast.wuxiaworld;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
-import androidx.navigation.NavDestination;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
@@ -24,8 +21,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class MainActivity extends AppCompatActivity implements  NavController.OnDestinationChangedListener{
+public class MainActivity extends AppCompatActivity {
 
+    public static final String HOME_URL = "https://inkeast.com";
+
+    private NavController mNavController;
     private Menu mMenu;
 
     private Map<Integer, Integer> mNavTopMap = new HashMap<>();
@@ -44,23 +44,27 @@ public class MainActivity extends AppCompatActivity implements  NavController.On
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.navigation_home, R.id.navigation_bookmark, R.id.navigation_history).build();
-        final NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        navController.addOnDestinationChangedListener(this);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+        mNavController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, mNavController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, mNavController);
 
         navView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 showMenu(item.getItemId());
-                return NavigationUI.onNavDestinationSelected(item, navController);
+                return NavigationUI.onNavDestinationSelected(item, mNavController);
             }
         });
     }
 
-    @Override
-    public void onDestinationChanged(@NonNull NavController controller, @NonNull NavDestination destination, @Nullable Bundle arguments) {
-        Toast.makeText(MainActivity.this, "onDestinationChanged", Toast.LENGTH_SHORT).show();
+    public void navigateToHome() {
+        mNavController.navigate(R.id.navigation_home);
+    }
+
+    public void navigateToHome(String url) {
+        Bundle bundle = new Bundle();
+        bundle.putString("url", url);
+        mNavController.navigate(R.id.navigation_home, bundle);
     }
 
     @Override
--- app/src/main/java/com/inkeast/wuxiaworld/ui/home/HomeFragment.java
+++ app/src/main/java/com/inkeast/wuxiaworld/ui/home/HomeFragment.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.inkeast.wuxiaworld.MainActivity;
 import com.inkeast.wuxiaworld.MainApplication;
 import com.inkeast.wuxiaworld.R;
 import com.inkeast.wuxiaworld.database.Bookmark;
@@ -21,11 +22,19 @@ import com.inkeast.wuxiaworld.database.History;
 
 public class HomeFragment extends Fragment {
 
-    private String url = "https://inkeast.com";
-
     private WebView mWebView;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        String url = null;
+        Bundle bundle = getArguments();
+        if (bundle != null) {
+            url = bundle.getString("url");
+            Toast.makeText(getContext(), url, Toast.LENGTH_SHORT).show();
+        }
+        if (url == null) {
+            url = MainActivity.HOME_URL;
+        }
 
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         mWebView = root.findViewById(R.id.webview);
